5b4da6cd6e61c7b880cfccea1e6bf40e29ee32e0
heitorMourao <heitor_41@hotmail.com>
2020-07-25 11:49:55 -0300

    Other methods calculator

--- ApiRest/Controllers/CalculatorController.cs
+++ ApiRest/Controllers/CalculatorController.cs
@@ -9,8 +9,8 @@ namespace ApiRest.Controllers
     [Route("api/[controller]")]
     public class CalculatorController : Controller
     {
-        // GET api/values
-        [HttpGet("{firstNumber}/{secondNumber}")]
+        // SOMA
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
         public IActionResult Sum(string firstNumber, string secondNumber)
         {
 
@@ -22,6 +22,79 @@ namespace ApiRest.Controllers
 
             return BadRequest ("Invalid input");
             
+        }
+
+        // SUBTRACAO
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtraction(string firstNumber, string secondNumber)
+        {
+
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input");
+
+        }
+
+        // GET api/values/division/5/5
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public IActionResult Division(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var division = CovertToDecimal(firstNumber) / CovertToDecimal(secondNumber);
+                return Ok(division.ToString());
+            }
+            return Ok("Invalid Input");
+        }
+
+        // GET api/values/multiplication/5/5
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var multiplication = CovertToDecimal(firstNumber) * CovertToDecimal(secondNumber);
+                return Ok(multiplication.ToString());
+            }
+            return Ok("Invalid Input");
+        }
+
+        // GET api/values/mean/5/5
+        [HttpGet("mean/{firstNumber}/{secondNumber}")]
+        public IActionResult Mean(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var mean = (CovertToDecimal(firstNumber) + CovertToDecimal(secondNumber)) / 2;
+                return Ok(mean.ToString());
+            }
+            return Ok("Invalid Input");
+        }
+
+        // GET api/values/square-root/5
+        [HttpGet("square-root/{number}")]
+        public IActionResult SquareRoot(string number)
+        {
+            if (IsNumeric(number))
+            {
+                var squareRoot = Math.Sqrt((double)CovertToDecimal(number));
+                return Ok(squareRoot.ToString());
+            }
+            return Ok("Invalid Input");
+        }
+
+        private decimal CovertToDecimal(string number)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
         }
 
         private decimal ConvertToDecimal(string number)
